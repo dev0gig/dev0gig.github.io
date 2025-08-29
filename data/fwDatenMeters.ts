5532e83be73fb3ccd8b51ead416b1f634d1d9c29
ギガ <115336867+dev0gig@users.noreply.github.com>
2025-08-29 12:19:53 +0200

    refactor: Migrate to ESM imports and clean up build configs
    This change updates the project to use ES Modules for all dependencies, simplifying import statements and leveraging CDN for faster loading. It also removes outdated deployment scripts and initial dummy data, preparing the project for a cleaner build and production readiness.

--- data/fwDatenMeters.ts
+++ data/fwDatenMeters.ts
@@ -1,22 +1,3 @@
 import { Meter } from '../components/FWDaten/types';
 
-export const initialMeters: Meter[] = [
-  {
-    id: 'meter-1',
-    number: '66168788',
-    purpose: 'WW',
-    location: 'Küche',
-  },
-  {
-    id: 'meter-2',
-    number: '66083241',
-    purpose: 'HZ',
-    location: 'Bad',
-  },
-  {
-    id: 'meter-3',
-    number: '66200989',
-    purpose: 'WW',
-    location: 'Bad',
-  },
-];
+export const initialMeters: Meter[] = [];
